dc150aa96c480a6d569bdd3424d9d3abfe85be26
Knetechs <knetechs@gmail.com>
2021-02-24 02:01:13 +0530

    thirdA

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My first app 1
+          My first app to test 1
         </a>
       </header>
     </div>
